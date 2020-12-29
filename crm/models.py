d5a550fdcd70409282a4c3ba420181425355e235
FloTheFlower <florentina.hofbauer@gmail.com>
2020-12-28 16:19:42 -0800

    feat: first migration

--- crm/models.py
+++ crm/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=400)
-    phone = models.CharField(max_length=400)
-    email = models.CharField(max_length=400)
+    name = models.CharField(max_length=400, null=True)
+    phone = models.CharField(max_length=400, null=True)
+    email = models.CharField(max_length=400, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
