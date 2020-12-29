0e43afbca93a7a5bb0faf435ff40d00e0324c380
FloTheFlower <florentina.hofbauer@gmail.com>
2020-12-28 16:41:39 -0800

    feat: change display in admin panel

--- crm/models.py
+++ crm/models.py
@@ -6,3 +6,5 @@ class Customer(models.Model):
     email = models.CharField(max_length=400, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
+def _str_(self):
+    return self.name
