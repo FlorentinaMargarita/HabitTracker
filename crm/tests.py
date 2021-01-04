cfdd66106283c186134b90edac4f46ea69bbeb82
FloTheFlower <florentina.hofbauer@gmail.com>
2021-01-04 10:26:33 -0800

    feat: wrote first unit test

--- crm/tests.py
+++ crm/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve    
+from crm.views import analytics, habit, count
 
-# Create your tests here.
+class TestUrls(SimpleTestCase):
+
+        def test_analytics_url_is_resolved(self): 
+            url = reverse('analytics')
+            print(resolve(url))
+            self.assertEquals(resolve(url).func, analytics)
+
+ 
