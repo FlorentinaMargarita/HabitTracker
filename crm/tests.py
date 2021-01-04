9c63914966a03ad3ccd4793fd7db234655a448d5
FloTheFlower <florentina.hofbauer@gmail.com>
2021-01-04 10:53:14 -0800

    feat: added a second view test

--- crm/tests.py
+++ crm/tests.py
@@ -13,12 +13,26 @@ class TestUrls(SimpleTestCase):
 
  
 class TestView(TestCase):
-        def test_order_get(self):
-            # this is the setup code 
-            client = Client()
-            # this is the actual test code 
-            response = client.get(reverse('analytics'))
+    # this function is going to be run before every single test method. It's used to setup a certain scenario.
+        def setUp(self):
+            self.client = Client()
+            self.analytics = reverse('analytics')
+            self.createHabit = reverse('create_habit')
+
+#here comes the actual test code. 
+
+        def test_analytics_get(self):
+           # Here we get access to the client we setup in the setup method.   
+            response = self.client.get(self.analytics)
             # here are the assertions
             self.assertEquals(response.status_code, 200)
             # This asserts that a certain response contains a specific template
             self.assertTemplateUsed(response, 'habit/analytics.html')
+
+        def test_create_habit_get(self):
+           # Here we get access to the client we setup in the setup method.   
+            response = self.client.get(self.createHabit)
+            # here are the assertions
+            self.assertEquals(response.status_code, 200)
+            # This asserts that a certain response contains a specific template
+            self.assertTemplateUsed(response, 'habit/order_form.html')
