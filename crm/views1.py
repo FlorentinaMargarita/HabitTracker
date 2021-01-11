0814b2043b0a67c3c7f079fdf7a881001eb8b5bb
FloTheFlower <florentina.hofbauer@gmail.com>
2021-01-11 09:46:35 -0800

    feat: added new dates

--- crm/views1.py
+++ crm/views1.py
@@ -7,23 +7,18 @@ from django.utils.dateparse import (
     parse_date, parse_datetime, parse_duration, parse_time
 )
 
-# here I get all the things from the database which I want to display in my dashboard.html
 def home(request):
     orders = Order.objects.all()
     total_orders = orders.count()
-    # "dailyfilter" is for showing all the habits which are daily
     dailyFilter = Order.objects.filter(interval="Daily")
-    #  "weeklyFilter" is for showing all the habits which are weekly
     weeklyFilter = Order.objects.filter(interval="Weekly")
-    # what ever is put in context will then be able to be displayed in the html-templates
     context = {'orders': orders, 'total_orders': total_orders, 'dailyFilter': dailyFilter, 'weeklyFilter':weeklyFilter}
     return render(request, 'habit/dashboard.html', context)
 
-# here I get all the things from the database which I want to display in my analytics.html
 def analytics(request):
     orders = Order.objects.all()
-    # with order.count I count the list of all current habits
-    total_orders = orders.count()   
+    total_orders = orders.count() 
+    # max_strikes = len(strikeList)
     context= {'total_orders': total_orders}
     return render(request, 'habit/analytics.html', context)
 
@@ -38,11 +33,14 @@ def habit(request, pk):
     order = Order.objects.get(id=pk)
     repeat = order.checkedList.filter()
     streak = order.streak
-    dateArray = order.checkedList.all().order_by('-dateAsString')
-    penultimate = dateArray[1].dateAsString
+    secondToLast = order.checkedList.all().order_by('-test')
+    print(secondToLast[1].test)
+    penultimate = secondToLast[1].test
     lastChecked = parse_date(penultimate)
-    today1 = dateArray.first().dateAsString
+    today1 = secondToLast.first().test
     today = parse_date(today1)
+    delta = today - lastChecked
+    print5 = print(delta)
     order.save()
     context = {"today": today, "lastTimeStamp": lastChecked, "testData": testData, "current_streak":streak, "order":order, "repeats": repeats, "repeat": repeat}
     return render(request, 'habit/habit.html', context)
@@ -94,7 +92,7 @@ def examplesStudy(request):
 
 def createHabit(request):
     form = OrderForm()
-    if request.method == 'POST':
+    if request.method == 'POST' :
         form = OrderForm(request.POST)
     if form.is_valid():
         form.save()
@@ -124,67 +122,26 @@ def delete(request, pk):
     context = {'item':order}
     return render(request, 'habit/delete.html', context)
 
-
-# the checkhabit function is the most important one of this project. 
-# Whenever a habit is checked, it creates a new instance of "Repeats", which stores the dateTime as a string in the database. 
-# The habit which was checked, stores this Repeats-object in the manytomany field. 
-
 def checkHabit(request, pk):
     order = Order.objects.get(id=pk)
     if request.method == 'POST':
         order.checked += 1
         myDateCheck = datetime.today().date()
-        # the line below creates a new Repeatsobject. 
-        # It stores the timedate at the second of when the habit was completed in terms of this project "checked"
-        newRep = Repeats.objects.create(dateAsString = myDateCheck)
-        # the line below adds the new Repeapts object in the manyToManyField called checkedList on the order.object. 
-        # This will later be used to compare the dates with one another.
+        newRep = Repeats.objects.create(test = myDateCheck)
         order.checkedList.add(newRep)
-        order.dateAsString = myDateCheck
-        # Here the array in which we will compare the dates to figure out streaks is created. 
-        # In order to compare the dates from today on and backwards the array has to be ordered reversed. 
-        # So with the latest added dates first. This is why it says order_by('-dateAsString'). The minus here reverses the string.
-        dateArray = order.checkedList.all().order_by('-dateAsString')
-        # Here we want the length of the array -1 to prevent the error "index out of range"
-        dateArray1 = int(len(order.checkedList.all().order_by('dateAsString')))-1
-        # These two indexes i and j are for counting up the current streaks.
+        order.test = myDateCheck
+        secondToLast = order.checkedList.all().order_by('-test')
         i = 1
         j = 0
-        latest = parse_date(dateArray[0].dateAsString)
-        previous_week = latest - timedelta(days=7)
         weekly = False
-        # These two indexes p and k are for counting up the longest streaks. However, as noted below, that doesnt work quite yet.
-        longest = order.longestStreak = 0
-        p = 1
-        k = 0
         streak = 0
-        max_count = 0 
-        prev_int = dateArray[p]
-        current = dateArray[k]
-        count = 0
-        difference = int(current.pk) - int(prev_int.pk)
-        # here I am finding the longest streak for the habits with the daily interval. However that solution doesnt work yet.
-        # while k < dateArray1 and order.interval == 'Daily':
-        #     if difference == 1:
-        #         count+=1
-        #         k+=1
-        #         p+=1
-        #         print(difference, "difference", current.pk, "currentPK", prev_int, "prevInt")
-        #     if count > max_count:
-        #         max_count = count
-        #         print("max_count loop", difference, "difference", current.pk, "currentPK", prev_int, "prevInt")
-        #     else:
-        #         count+=1
-        #         k+=1
-        #         p+=1
-        #         print("else loop")
-        #     order.longestStreak = max_count
-
-        for pointInTime in dateArray:
+        latest = parse_date(secondToLast[0].test)
+        previous_week = latest - timedelta(days=7)
+        for oneThing in secondToLast:
             if order.interval == "Daily":
-                penultimate = pointInTime.dateAsString
+                penultimate = oneThing.test
                 lastChecked = parse_date(penultimate)
-                previous_day = dateArray[i].dateAsString
+                previous_day = secondToLast[i].test
                 previous_day = parse_date(previous_day)
                 newStreak = lastChecked - previous_day
                 if newStreak.days == 1:
@@ -193,7 +150,7 @@ def checkHabit(request, pk):
                     break
                 i += 1
             elif order.interval == "Weekly":
-                date = parse_date(pointInTime.dateAsString)
+                date = parse_date(oneThing.test)
                 if weekly:
                     previous_week = previous_week - timedelta(days=7)
                     weekly = False
@@ -202,7 +159,7 @@ def checkHabit(request, pk):
                     weekly = True
                     latest = date
                     try:
-                        if (latest - parse_date(dateArray[j + 1].dateAsString)).days > 7:
+                        if (latest - parse_date(secondToLast[j + 1].test)).days > 7:
                             break
                     except:
                         pass
@@ -210,8 +167,10 @@ def checkHabit(request, pk):
         order.streak = streak
         order.save()
         return redirect('/')
+
+
     order.save()
     return redirect('/')
-    context = {"longest": order.longest, 'checked': order.checked, 'myDateCheck': myDateCheck, "repeats": repeats}
+    context = {'checked': order.checked, 'myDateCheck': myDateCheck, "repeats": repeats}
     return render(request, 'habit/order_form.html', context)
 
