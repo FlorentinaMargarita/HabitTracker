0224483a4c9ec8537559e7af373e033fe1ac54a6
FloTheFlower <florentina.hofbauer@gmail.com>
2021-01-31 08:41:32 -0800

    feat: tests are running

--- crm/tests.py
+++ crm/tests.py
@@ -11,6 +11,12 @@ class TestUrls(SimpleTestCase):
             print(resolve(url))
             self.assertEquals(resolve(url).func, analytics)
 
+# # TestCase is in the testing framework. It is from Django. Most of the testCases will inherit from TestCase by default.
+# # So what is happening is the following, if we use the Django testFramework: 
+# # 1.) It will run all the tests which are in the main app under the tests.py file. 
+# # 2.) If the test run succesfully it will tell you "destroying test database". Here Django is creating a SQLite DB. It is putting this
+# #     in memory, running everything and then destroying this database out of memory. Because it is in memory it is really fast. 
+
 
 # I set up different things that I need, for each of the tests
 class TestModels(TestCase):
@@ -20,46 +26,30 @@ class TestModels(TestCase):
             interval='Weekly'
         )
 
- 
-# class TestView(TestCase):
-#     # this function is going to be run before every single test method. It's used to setup a certain scenario.
-#         def setUp(self):
-#             self.client = Client()
-#             self.analytics = reverse('analytics')
-#             self.createHabit = reverse('create_habit')
-
-# # TestCase is in the testing framework. It is from Django. Most of the testCases will inherit from TestCase by default.
-# # So what is happening is the following, if we use the Django testFramework: 
-# # 1.) It will run all the tests which are in the main app under the tests.py file. 
-# # 2.) If the test run succesfully it will tell you "destroying test database". Here Django is creating a SQLite DB. It is putting this
-# #     in memory, running everything and then destroying this database out of memory. Because it is in memory it is really fast. 
-class BasicTest(TestCase):
-        def test_field(self):
-            habit = Order()
-            habit.habit = "Write tests"
-            habit.interval = "Daily"
-            habit.save()
-
-            record = Order.objects.get(pk = habit.pk)
-            self.assertEqual(record, habit)
+class TestView(TestCase):
+    # this function is going to be run before every single test method. It's used to setup a certain scenario.
+        def setUp(self):
+            self.client = Client()
+            self.analytics = reverse('analytics')
+            self.createHabit = reverse('create_habit')
 
 #here comes the actual test code. 
 
-        # def test_analytics_get(self):
-        #    # Here we get access to the client we setup in the setup method.   
-        #     response = self.client.get(self.analytics)
-        #     # here are the assertions
-        #     self.assertEquals(response.status_code, 200)
-        #     # This asserts that a certain response contains a specific template
-        #     self.assertTemplateUsed(response, 'habit/analytics.html')
+        def test_analytics_get(self):
+           # Here we get access to the client we setup in the setup method.   
+            response = self.client.get(self.analytics)
+            # here are the assertions
+            self.assertEquals(response.status_code, 200)
+            # This asserts that a certain response contains a specific template
+            self.assertTemplateUsed(response, 'habit/analytics.html')
 
-        # def test_create_habit_get(self):
-        #    # Here we get access to the client we setup in the setup method.   
-        #     response = self.client.get(self.createHabit)
-        #     # here are the assertions
-        #     self.assertEquals(response.status_code, 200)
-        #     # This asserts that a certain response contains a specific template
-        #     self.assertTemplateUsed(response, 'habit/order_form.html')
+        def test_create_habit_get(self):
+           # Here we get access to the client we setup in the setup method.   
+            response = self.client.get(self.createHabit)
+            # here are the assertions
+            self.assertEquals(response.status_code, 200)
+            # This asserts that a certain response contains a specific template
+            self.assertTemplateUsed(response, 'habit/order_form.html')
 
 
 # Tests
